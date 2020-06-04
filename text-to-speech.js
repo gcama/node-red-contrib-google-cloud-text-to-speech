a2a534ab5b31eecb4180fc0662b9ff366fb4162e
kosuke <kosuke@MacBook-Pro.local>
2020-06-04 15:11:16 +0900

    edit

--- text-to-speech.js
+++ text-to-speech.js
@@ -2,6 +2,8 @@ module.exports = function(RED) {
     "use strict";
     const NODE_TYPE = "google-cloud-text-to-speech";
     const textToSpeech = require('@google-cloud/text-to-speech');
+    const fs = require('fs');
+    const util = require('util');
 
     function TextToSpeechNode(config) {
         RED.nodes.createNode(this, config);
@@ -14,6 +16,8 @@ module.exports = function(RED) {
         const voicePitch         = config.voicePitch         || -3.1;
         const voiceSpeakingRate  = config.voiceSpeakingRate  || 1.1;
         const voiceAudioEncoding = config.voiceAudioEncoding || "MP3";
+        const voiceDir           = config.voiceDir           || "/home/pi/";
+        const voiceFileName      = config.voiceFileName      || "audio.mp3";
 
         if (config.account) {
             credentials = GetCredentials(config.account);
@@ -46,7 +50,8 @@ module.exports = function(RED) {
                     audioEncoding: voiceAudioEncoding,
                     effectsProfileId: ['medium-bluetooth-speaker-class-device'],
                     pitch: voicePitch,
-                    speakingRate: voiceSpeakingRate
+                    speakingRate: voiceSpeakingRate,
+                    sampleRateHertz: 24000
                 }
             };
 
@@ -54,8 +59,10 @@ module.exports = function(RED) {
                 node.status({fill: "blue", shape: "dot", text: "processing"});
                 const [response] = await textToSpeechClient.synthesizeSpeech(request);
                 node.status({});
-                msg.payload = response.audioContent;
+                const writeFile = util.promisify(fs.writeFile);
+                await writeFile(voiceDir + voiceFileName, response.audioContent, 'binary');
                 node.log(msg);
+                msg.payload = voiceDir + voiceFileName;
                 node.send(msg);
             }
             catch(exp) {
